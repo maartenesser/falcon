b0a7bb847d6ec7427e59f34069a3536f82c4548a
Romeo Saint-Albin <r.stalbin@gmail.com>
2019-05-28 15:41:59 +0200

    drop insurance table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_120023) do
+ActiveRecord::Schema.define(version: 2019_05_28_133358) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,18 +27,9 @@ ActiveRecord::Schema.define(version: 2019_05_28_120023) do
     t.string "number"
     t.datetime "at_date"
     t.text "description"
-    t.bigint "insurance_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
-    t.index ["insurance_id"], name: "index_claims_on_insurance_id"
-  end
-
-  create_table "insurances", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "orders", force: :cascade do |t|
@@ -79,7 +70,6 @@ ActiveRecord::Schema.define(version: 2019_05_28_120023) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "claims", "insurances"
   add_foreign_key "claims", "users"
   add_foreign_key "orders", "users"
   add_foreign_key "parts", "cars"

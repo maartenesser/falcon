457a1701a8963b8689014e8f0c7150df67e3dfe0
Romeo Saint-Albin <r.stalbin@gmail.com>
2019-05-28 18:29:23 +0200

    conflct

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-<<<<<<< HEAD
   resources :orders, only: [ :show]
-=======
   resources :parts, only: [:create, :update]
->>>>>>> master
 end

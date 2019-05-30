2784804e8c95099871854e68374322d20856d00d
Nathanael Alain <nathanaelalain@yahoo.fr>
2019-05-31 00:12:09 +0200

    update the view from the graphs

--- app/javascript/packs/charts/donutPie.js
+++ app/javascript/packs/charts/donutPie.js
@@ -5,69 +5,47 @@ const ctx_pie = document.querySelector('#myPieChart');
 const ctx_bar = document.querySelector('#myBarChart');
 
   new Chart(ctx_pie, {
-    type: 'pie',
+    type: 'doughnut',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Lighting', 'Body', 'Electric', 'Sensors'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: '# of Categories',
+            data: [12, 19, 3, 5],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 0
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+                'rgba(75, 192, 192, 0.2)'
 
-  new Chart(ctx_bar, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(75, 192, 192, 1)'
             ],
             borderWidth: 1
         }]
     },
     options: {
+
+    }
+  });
+
+  new Chart(ctx_bar, {
+    type: 'line',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'Juni'],
+        datasets: [{
+            label: 'Sold Parts',
+            data: [1, 3, 2, 5, 8, 15],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)'
+                ],
+
+        }]
+    },
+    options: {
         scales: {
             yAxes: [{
                 ticks: {
@@ -76,7 +54,7 @@ const ctx_bar = document.querySelector('#myBarChart');
             }]
         }
     }
-});
+  });
 
 }
 

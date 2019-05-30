7f0c53a924d9a3ff382edd0bd2432bd464f75033
Nathanael Alain <nathanaelalain@yahoo.fr>
2019-05-30 18:59:12 +0200

    first set up for chart.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,10 @@ import "bootstrap";
 // import "./dataTables/tables";
 import "./sb-admin-2/sb-admin-2";
 
+import testFunction from './charts/donutPie';
+
 import tables from './data_tables/tables.js';
 
 tables();
+
+testFunction();
---
+++ app/javascript/packs/charts/donutPie.js
@@ -0,0 +1,9 @@
+
+const testFunction = (data) => {
+
+  // let data = document.querySelector('.donut').data('stats');
+
+console.log('Hello from the other side');
+}
+
+export default testFunction;

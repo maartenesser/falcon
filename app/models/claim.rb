d4779037a59aa355de36b29218ab9fd570f807e8
Gael Tims <gaeltims@gmail.com>
2019-05-30 19:49:06 +0200

    first commit for search function - part search is working

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -5,4 +5,16 @@ class Claim < ApplicationRecord
   validates :number, presence: true
   validates :at_date, presence: true
   validates :description, presence: true
+
+  include PgSearch
+  pg_search_scope :global_search,
+    against: %i[number at_date description],
+    associated_against: {
+      parts: %i[part_model_code category title description],
+      cars: %i[make model vin]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
+
 end

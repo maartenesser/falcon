95207065753a967ad73250ee84f2df07b19e7e55
Romeo Saint-Albin <r.stalbin@gmail.com>
2019-05-30 13:55:23 +0200

    Remove typo from table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_112637) do
+ActiveRecord::Schema.define(version: 2019_05_30_115330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 2019_05_30_112637) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "part_id"
+    t.index ["part_id"], name: "index_orders_on_part_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 

d7a57cd7fce9fecd3ba2069f28d47ca8d2023793
Nathanael Alain <nathanaelalain@yahoo.fr>
2019-05-31 15:01:42 +0200

    first set of real statistics

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,14 @@ import "bootstrap";
 // import "./dataTables/tables";
 import "./sb-admin-2/sb-admin-2";
 
-import testFunction from './charts/donutPie';
+import graphFunction from './charts/donutPie';
 
 import tables from './data_tables/tables.js';
 import scrollToTop from './easyOutScroll.js';
 
 tables();
 
-testFunction();
+graphFunction();
 
 let scroller = document.querySelector('.scroll-to-top').addEventListener("click", (event) => {
   scrollToTop(1000);

6e49cda0687e2ae79ffd530f3ba244895db422c6
Nathanael Alain <nathanaelalain@yahoo.fr>
2019-06-01 01:36:59 +0200

    add more real data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,16 +7,23 @@ class PagesController < ApplicationController
         @part_total = Part.count
         @part_earning_total_insurance = Money.new(current_user.parts_as_insurance.sum(:price_cents), 'EUR')
       else
-        parts_bought = []
-        parts = current_user.orders.where(status: "pending")
-        parts.each do |part|
-          parts_bought << part
+        parts_bought = 0
+        orders = current_user.orders
+        orders.each do |order|
+          parts_bought += order.part.price_cents
         end
-          #@parts_bought_sum = Money.new(parts_bought.sum(:price_cents), 'EUR')
-        # @part_earning_total_garage = Money.new(current_user.find().sum(:price_cents), 'EUR')
+        @my_parts_bought = Money.new(parts_bought, 'EUR')
+
+
+
+        # @part_earning_total_garage = Money.new(current_user.parts_as_garage.sum(:price_cents), 'EUR')
+        @my_parts_sell = Money.new(current_user.parts.sum(:price_cents), 'EUR')
+
+        #@my_parts_bought = Money.new(parts_bought.sum(:price_cents), 'EUR')
+
         @part_total = Part.count
         @pending_orders = current_user.orders.where(status: "pending").count
-        # Order.joins(:Parts).where(articles: { author: author })
+
       end
     end
   end

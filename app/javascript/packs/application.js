42e4f73fe643d0ccfd85b9382eba61e1d6ed9c6e
Maarten Esser <maartenjr.esser@gmail.com>
2019-06-01 13:43:37 +0200

    midified some js funcktions and added migrations

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,21 +1,18 @@
 import "bootstrap";
 import "./sb-admin-2/sb-admin-2";
-
 import graphFunction from './charts/donutPie';
-
 import tables from './data_tables/tables.js';
 import scrollToTop from './easyOutScroll.js';
 
 tables();
 
-graphFunction();
-
 let scroller = document.querySelector('.scroll-to-top').addEventListener("click", (event) => {
   scrollToTop(1000);
 })
 
-//function to hide the up button of the page. and only show if you scroll down
+  // function to hide the up button of the page. and only show if you scroll down
 $(document).ready(function(){
+  console.log("showing or not showing pagup")
     //Check to see if the window is top if not then display button
     $(window).scroll(function(){
       if ($(this).scrollTop() > 100) {
@@ -24,10 +21,18 @@ $(document).ready(function(){
         $('.scrollToTop').fadeOut();
       }
     });
-
     //Click event to scroll to top
     $('.scrollToTop').click(function(){
       $('html, body').animate({scrollTop : 0},800);
       return false;
     });
 });
+
+//function to hide the notification after a 1 second
+$(function() {
+  setTimeout(function(){
+    $('.alert').slideUp(500);
+  }, 1000);
+});
+
+graphFunction();

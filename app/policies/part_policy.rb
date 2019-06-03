cfc1f633dcdd1e26d0911408f3efa22eb4058323
Romeo Saint-Albin <r.stalbin@gmail.com>
2019-06-03 11:30:12 +0200

    Commit before merge

--- app/policies/part_policy.rb
+++ app/policies/part_policy.rb
@@ -2,8 +2,6 @@ class PartPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       scope.left_outer_joins(:order).where.not(user: user).where(orders: { status: "pending" }).or(Part.left_outer_joins(:order).where.not(user: user).where(orders: { id: nil }))
-      # scope define to see all the parts except mine
-      # can we scope with status as well..
     end
   end
 

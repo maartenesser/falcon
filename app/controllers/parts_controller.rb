eebdceb5bd98b9dc0ac65611972471a561a6b1ce
Romeo Saint-Albin <r.stalbin@gmail.com>
2019-06-06 13:25:45 +0200

    ISNCEUD

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -1,24 +1,6 @@
 class PartsController < ApplicationController
   before_action :set_part, only: [:show, :edit, :update]
 
-  def show
-    # The logic below will have to be implemented and we will have to raise
-    # an error if a user tries to access his part like that
-    # But the selling parts controller will have to be checked first
-    # part = Part.where.not(user: current_user)
-    # @part = part.find(params[:id])
-
-    user_id = @part.user_id
-    @user = User.find(user_id)
-    claim_id = @part.claim_id
-    @claim = Claim.find(claim_id)
-
-
-    car_id = @part.car_id
-    @car = Car.find(car_id)
-    # raise
-  end
-
   def index
     @parts = policy_scope(Part).order(created_at: :desc)
     if params[:query].present?
@@ -26,25 +8,21 @@ class PartsController < ApplicationController
     end
   end
 
+  def show
+    user_id = @part.user_id
+    @user = User.find(user_id)
+
+    claim_id = @part.claim_id
+    @claim = Claim.find(claim_id)
+
+    car_id = @part.car_id
+    @car = Car.find(car_id)
+  end
+
   def new
     @part = Part.new
     authorize @part
   end
-
-  def update
-    # if !@part.order.nil? && @part.order.user_id == current_user.id
-    #   notice: 'You cannot buy the same part twice'
-    #   redirect_to root_path
-    # end
-
-    if @part.update(part_params)
-      redirect_to part_path(@part)
-    else
-      render :edit
-    end
-  end
-
-  def edit; end
 
   def create
     @part = Part.new(part_params)
@@ -58,8 +36,17 @@ class PartsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @part.update(part_params)
+      redirect_to part_path(@part)
+    else
+      render :edit
+    end
+  end
+
   def destroy
-    # @part.destroy
     part = Part.find(@part)
     part.destroy
     redirect_to parts_path
